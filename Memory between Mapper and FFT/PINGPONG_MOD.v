25a5df75bfca0b7df6227859dc8387c533e0cfc8
ZeyadNar <zeyadnar88@gmail.com>
2024-06-19 20:22:36 +0300

    Update PINGPONG_MOD.v
    Updated to adapt Busy signal from FFT , save two Last addr assuming no of mod_done will not exceed 2 before fft finishes , so we must be sure that the ping pong will not be triggered before FFT finishes ping 
    
    Not sure if the memory is synthizable :')

--- Memory between Mapper and FFT/PINGPONG_MOD.v
+++ Memory between Mapper and FFT/PINGPONG_MOD.v
@@ -6,6 +6,7 @@ module PingPongMem_MOD #(
     input wire RST,
 
     input wire [DATA_WIDTH-1:0] data_in,
+    input wire BUSY , 
     input wire [10:0] Last_addr,
     input wire write_enable,
     input wire Mod_Valid_OUT ,
@@ -16,14 +17,17 @@ module PingPongMem_MOD #(
 );
 
     // Memories
-    
+    reg  [10:0] Last_addr_mem [1:0] ; 
     reg signed [DATA_WIDTH-1:0] ping [0:MEM_DEPTH-1];
     reg signed [DATA_WIDTH-1:0] pong [0:MEM_DEPTH-1];
     reg [10:0] Last_indx ; 
     reg flag ; 
+    reg [1:0] Counter ; 
     // Memory select flag
     reg use_ping;
     integer i;
+    integer k ; 
+    reg out_ping ; 
     // Memory select and reset logic
     
      always @(*) begin
@@ -33,7 +37,13 @@ module PingPongMem_MOD #(
             use_ping = ~use_ping;
         end
     end
-    
+    always@(posedge CLK or negedge RST) begin 
+        if(! RST )
+            out_ping <= 1 ; 
+        else if(Last_indx == Last_addr_mem[0]) begin 
+            out_ping <= ~out_ping;
+        end    
+    end    
     
     always @(posedge CLK or negedge RST) begin
         if (!RST) begin
@@ -42,7 +52,7 @@ module PingPongMem_MOD #(
                     ping[i] <=0 ;
              end
         end 
-        else if (MOD_DONE) begin
+        else if (MOD_DONE && (Counter == 0)) begin
             if(use_ping) begin 
                 for (i = 0; i < MEM_DEPTH; i = i + 1) begin
                     ping[i] <=0 ;
@@ -75,18 +85,26 @@ module PingPongMem_MOD #(
      if(! RST) begin 
           data_out <= 0 ; 
           Last_indx <= 1 ;   
-     end
-    else if (MOD_DONE) begin 
+     end    
+    else if (MOD_DONE && (Counter == 0) ) begin 
             data_out <= 'b0 ;
             Last_indx <= 1 ;  
     end    
-     else if (flag) begin
-            if (use_ping) begin
-                data_out <= pong[Last_indx-1];
+     else if (flag & ! BUSY) begin
+            // if done counter > 1 , but we havent reached the last index yet continue to output from 
+            if (out_ping ) begin
+                 data_out <= ping[Last_indx-1];            
             end else begin
-                data_out <= ping[Last_indx-1];
-            end
-            Last_indx <= Last_indx +1 ; 
+                 data_out <= pong[Last_indx-1];
+            end   
+            
+         // index for ouput return to initial value every time it reaches the last_Addr   
+            if(Last_indx == Last_addr_mem[0] ) begin 
+                Last_indx = 1 ;
+            end else begin 
+                Last_indx <= Last_indx +1 ; 
+            end 
+  
      end   else begin
             data_out <= 'b0 ;
             Last_indx <= 1 ;  
@@ -94,12 +112,42 @@ module PingPongMem_MOD #(
     end
  
 always@(posedge CLK or negedge RST) begin 
-    if(!RST)
-            flag = 0 ; 
+    if(!RST) begin 
+            Counter <= 0 ; 
+            flag <= 0 ;      
+    end
     else if(MOD_DONE) begin 
-            flag = 1 ; 
-    end else if (Last_indx == Last_addr)  
-            flag = 0 ; 
+            Counter <= Counter + 1 ; 
+            flag <= 1 ; 
+    end
+     else if (Last_indx == Last_addr_mem[0]  ) begin  
+                 Counter <= Counter - 1; 
+     end           
+                 if ( (Counter == 0)) begin 
+                        flag <= 0 ; 
+                 end
+                 else begin 
+                        flag <= 1 ;  
+                 end          
+    
+end
 
+
+always @(posedge CLK or negedge RST) begin 
+    if(!RST) begin 
+        Last_addr_mem[0] <= 0 ; 
+        Last_addr_mem[1] <= 0 ; 
+        k <= 0 ; 
+    end
+    else if(MOD_DONE) begin
+        Last_addr_mem[k] <= Last_addr ; 
+        k <= k +1 ; 
+    end else if (Last_indx == Last_addr_mem[0]) begin 
+         Last_addr_mem [0] <= Last_addr_mem[1] ;        
+         Last_addr_mem [1] <= 0 ;   
+         k <= 0 ;      
+    end
 end    
+
+   
 endmodule
